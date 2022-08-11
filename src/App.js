8163264283d54e019cf6fee30382471843d8a4ac
Chung Nguyen <zrmedia9@gmail.com>
2022-08-11 21:07:44 +0700

    Done prepare project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl font-bold">Hello world!</h1>
+      <h1 className="text-4xl font-bold font-signature">Chung Nguyen</h1>
     </div>
   );
 }
